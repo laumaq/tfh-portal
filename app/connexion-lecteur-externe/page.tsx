734945020cb68ff7847bb72398cad89355777484
laumaq <wod58@hotmail.com>
2026-01-14 10:01:58 +0100

    Update page.tsx

--- app/connexion-lecteur-externe/page.tsx
+++ app/connexion-lecteur-externe/page.tsx
@@ -4,7 +4,6 @@
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { supabase } from '@/lib/supabase';
-import WelcomeMessage from '@/components/WelcomeMessage';
 
 export default function LoginLecteurExternePage() {
   const [nom, setNom] = useState('');
