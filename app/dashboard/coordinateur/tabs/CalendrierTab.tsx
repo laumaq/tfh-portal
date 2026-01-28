8d6cd0d0ae7a6a5955172b24fde412f7106389e3
laumaq <wod58@hotmail.com>
2026-01-28 10:08:21 +0100

    Update CalendrierTab.tsx

--- app/dashboard/coordinateur/tabs/CalendrierTab.tsx
+++ app/dashboard/coordinateur/tabs/CalendrierTab.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect, useCallback, useMemo  } from 'react';
 import CalendarDisplay from '@/app/components/CalendarDisplay';
 import ConflictDisplay from '../components/ConflictDisplay';
 import { Eleve, DefenseEvent, Conflict } from '../types';
